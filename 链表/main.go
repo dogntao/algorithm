3bc45a4c438437b1b418670f59e1e14fcf79bff8
dongt <dongt@dongtaodeMac-mini.local>
2020-02-19 18:38:47 +0800

    增加链表

--- 链表/main.go
+++ 链表/main.go
@@ -1,48 +1,62 @@
 package main
 
-import "fmt"
-
 type ListNode struct {
-	val  int
-	next *ListNode
+	Val  int
+	Next *ListNode
 }
 
-// 翻转链表
-func flip(head *ListNode) *ListNode {
-	cur := head
+/*
+ * 1、链表头部 pre
+ * 2、已处理尾部 tail
+ * 2、已翻转局部头部 ph和尾部 pt
+ * 3、即将翻转头部 cur
+ */
+func reverseKGroup(head *ListNode, k int) *ListNode {
 	var pre *ListNode = nil
+	var tail *ListNode = nil
+	var ph *ListNode = nil
+	var pt *ListNode = nil
+	cur := head
 	for cur != nil {
-		cur, cur.next, pre = cur.next, pre, cur
+		j := 0
+		// 构造局部翻转链表
+		var listNodeTmp *ListNode = nil
+		for i := 0; i < k; i++ {
+			j++
+			listNodeTmp = cur
+			cur = cur.Next
+		}
+		// k个一组翻转链表
+		if j%k == 0 {
+			ph = reverseList(listNodeTmp)
+			pt = listNodeTmp
+			if tail != nil {
+				tail.Next = ph
+			}
+			tail = pt
+		} else {
+			// 不足k个不翻转
+			ph = listNodeTmp
+			tail.Next = ph
+		}
+		// 初始化链表头
+		if pre == nil {
+			pre = ph
+		}
 	}
 	return pre
 }
 
-func main() {
-	head := &ListNode{val: 0}
-	lisNode1 := &ListNode{val: 1}
-	lisNode2 := &ListNode{val: 2}
-	lisNode3 := &ListNode{val: 3}
-	lisNode4 := &ListNode{val: 4}
-	lisNode5 := &ListNode{val: 5}
-
-	head.next = lisNode1
-	lisNode1.next = lisNode2
-	lisNode2.next = lisNode3
-	lisNode3.next = lisNode4
-	lisNode4.next = lisNode5
-
-	fmt.Println(head)
-	fmt.Println(head.next)
-	fmt.Println(head.next.next)
-	fmt.Println(head.next.next.next)
-	fmt.Println(head.next.next.next.next)
-	fmt.Println(head.next.next.next.next.next)
-
-	pre := flip(head)
-	fmt.Println(pre)
-	fmt.Println(pre.next)
-	fmt.Println(pre.next.next)
-	fmt.Println(pre.next.next.next)
-	fmt.Println(pre.next.next.next.next)
-	fmt.Println(pre.next.next.next.next.next)
+/*
+ * 翻转链表
+ * pre链表头部，cur当前翻转位置
+ * cur.Next=pre,pre=cur,cur=cur.Next
+ */
+func reverseList(head *ListNode) *ListNode {
+	var pre *ListNode = nil
+	cur := head
+	for cur != nil {
+		cur.Next, pre, cur = pre, cur, cur.Next
+	}
+	return pre
 }
